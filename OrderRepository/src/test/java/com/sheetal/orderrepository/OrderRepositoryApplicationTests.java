9598f03ee046319c5599ee9879357f32082cbfdc
codetalks95 <info@codetalksdna.com>
2024-02-02 10:50:36 +0530

    Added repositories

---
+++ OrderRepository/src/test/java/com/sheetal/orderrepository/OrderRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheetal.orderrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
