9a2e872a70f95ac74fe0a5976de43172b5aa832b
codetalks95 <info@codetalksdna.com>
2024-02-06 11:48:41 +0530

    Added Resilience4j

--- OrderService/src/main/java/com/sheetal/orderservice/Controller/OrderController.java
+++ OrderService/src/main/java/com/sheetal/orderservice/Controller/OrderController.java
@@ -3,6 +3,7 @@ package com.sheetal.orderservice.Controller;
 import com.sheetal.orderservice.Entity.Employee;
 import com.sheetal.orderservice.FeignClient.FeignClientRestCall;
 import com.sheetal.orderservice.RestTemplate.RestTemplateConfiguration;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,8 +38,19 @@ public class OrderController {
     public String getEmployeeInfoFromFeignClient(@RequestBody Employee employee) {
         return feignClientRestCall.getEmployeeInformation(employee);
     }
+
     @PostMapping("/getEmployeeInfoFromRestTemplate")
     public String getEmployeeInfoFromRestTemplate(@RequestBody Employee employee) {
         return restTemplateConfiguration.getEmployeeInformation(employee);
+    }
+
+    @PostMapping("/getEmployeeInfoFromRestTemplateFaultTolerance")
+    @CircuitBreaker(name = "Fault Tolerance", fallbackMethod = "faultTolerance")
+    public String getEmployeeInfoFromRestTemplateFaultTolerance(@RequestBody Employee employee) {
+        throw new RuntimeException();
+    }
+
+    public String faultTolerance(Throwable throwable) {
+        return "Hey Dude" + " " + "the service is down please come back Later or Give it a Retry!!";
     }
 }
